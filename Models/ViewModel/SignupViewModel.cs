1b782d88e16273609c04b3a0e0de48f722fac3c8
Takuto <tiharu717@gmail.com>
2023-08-13 09:30:25 +0900

    タグヘルパーを使用する形に変更 https://learn.microsoft.com/ja-jp/aspnet/core/mvc/views/working-with-forms?view=aspnetcore-7.0#the-label-tag-helper

--- Models/ViewModel/SignupViewModel.cs
+++ Models/ViewModel/SignupViewModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace webUserLoginTest.Models.ViewModel;
 
 public class SignupViewModel
 {
+    [Display(Name = "名前")]
     public string Name { get; set; }
     public string Password { get; set; }
 }
